074b953f2af10191144e6b170da13e9c7f7010ac
Andrei Brehar <tnsteve123@gmail.com>
2022-05-03 13:45:48 +0200

    new

--- pages/scripts/stopwatch.js
+++ pages/scripts/stopwatch.js
@@ -1,39 +1,40 @@
 const timer = document.getElementById('time');
-const resume=document.getElementById('resume');
-const stop=document.getElementById('stops');
-const milis=document.getElementById('mili');
-const reset=document.getElementById('reset');
+const resume = document.getElementById('resume');
+const stop = document.getElementById('stops');
+const milis = document.getElementById('mili');
+const reset = document.getElementById('reset');
 var count = 0;
 var hr = 0;
 var min = 0;
-var sec =0;
+var sec = 0;
 var mil = 0;
 var stoptime = true;
-reset.style.opacity='50%';
-stop.style.opacity='50%';
+reset.style.opacity = '50%';
+stop.style.opacity = '50%';
 
 function startTimer() {
 
-    reset.style.opacity='100%';
+    reset.style.opacity = '100%';
     count++;
     if (stoptime == true) {
         stoptime = false;
-        resume.innerHTML='Start';
-        resume.style.opacity='50%';
+        resume.innerHTML = 'Start';
+        resume.style.opacity = '50%';
         timerCycle();
     }
-    if(count!=0){
-        stop.style.opacity='100%';
+    if (count != 0) {
+        stop.style.opacity = '100%';
     }
 }
+
 function stopTimer() {
-    if(count!=0){
+    if (count != 0) {
         var count = 0;
-        resume.innerHTML='Resume';
-        resume.style.opacity='100%';
+        resume.innerHTML = 'Resume';
+        resume.style.opacity = '100%';
     }
-    if(count==0){
-        stop.style.opacity='50%';
+    if (count == 0) {
+        stop.style.opacity = '50%';
     }
     if (stoptime == false) {
         stoptime = true;
@@ -46,7 +47,7 @@ function timerCycle() {
         min = parseInt(min);
         hr = parseInt(hr);
 
-        sec=sec+1;
+        sec = sec + 1;
 
         if (sec == 60) {
             min = min + 1;
@@ -69,37 +70,39 @@ function timerCycle() {
         }
 
         timer.innerHTML = hr + ':' + min + ':' + sec;
-        
+
 
         setTimeout("timerCycle()", 1000);
     }
 }
-function miliTimer(){
-    if(stoptime==false){
+
+function miliTimer() {
+    if (stoptime == false) {
         mil++;
 
-    
-        if(mil<10 || mil==0){
-            mil='0'+mil;
+
+        if (mil < 10 || mil == 0) {
+            mil = '0' + mil;
         }
-        if(mil==99){
-            mil=0;
+        if (mil == 99) {
+            mil = 0;
         }
-        milis.innerHTML=':' + mil;
+        milis.innerHTML = ':' + mil;
     }
 }
 setInterval(miliTimer, 10);
+
 function resetTimer() {
-    hr=0;
-    min=0;
-    sec=0;
-    mil=0;
+    hr = 0;
+    min = 0;
+    sec = 0;
+    mil = 0;
     timer.innerHTML = '00:00:00';
-    milis.innerHTML=':0'+mil;
-    resume.innerHTML='Start';
-    resume.style.opacity='100%';
-    stop.style.opacity='50%';
-    reset.style.opacity='50%';
-    stoptime=true;
-    
+    milis.innerHTML = ':0' + mil;
+    resume.innerHTML = 'Start';
+    resume.style.opacity = '100%';
+    stop.style.opacity = '50%';
+    reset.style.opacity = '50%';
+    stoptime = true;
+
 }
